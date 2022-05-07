1305ff2e11af3acdc8c042c387224cde065dac7b
Adarsh Malviya <75481003+adarshmalviy@users.noreply.github.com>
2022-05-07 11:09:59 +0530

    maximum-product-subarray

---
+++ maximum-product-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans  = nums[0];
+        int maxi =  ans;
+        int mini = ans;
+
+        for (int i =1; i< nums.size();i++)
+        {
+            if(nums[i]<0)   swap(maxi,mini);
+
+            maxi = max(nums[i], nums[i] * maxi);
+            mini = min(nums[i], nums[i] * mini);
+            ans = max(maxi,ans);
+        }
+        return ans;
+    }
+};
